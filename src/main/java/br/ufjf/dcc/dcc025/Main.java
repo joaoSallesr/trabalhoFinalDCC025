f6d12be15ef64b5d1d4d2e9a4a259e53a400136c
Julia <juliacamposdp.gmail.com>
2026-01-09 18:12:17 -0300

    Controller recepcionista e Cadastro

--- src/main/java/br/ufjf/dcc/dcc025/Main.java
+++ src/main/java/br/ufjf/dcc/dcc025/Main.java
@@ -1,40 +1,48 @@
 package br.ufjf.dcc.dcc025;
 
 import br.ufjf.dcc.dcc025.model.repository.GerenciadorRepository;
+import br.ufjf.dcc.dcc025.view.LoginView;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Teste");
-        GerenciadorRepository.getInstance(); // Cria o repositório para o sistema atual
-        //new RecepcionistaView().setVisible(true);
+        public static void main(String[] args) {
+                System.out.println("Teste");
+                GerenciadorRepository.getInstance(); // Cria o repositório para o sistema atual
+                // new RecepcionistaView().setVisible(true);
+                new LoginView().setVisible(true);
 
-        /*
-        // TESTE DE PERSISTÊNCIA
-        Nome recNome = new Nome("Teste", "Recepcionista");
-        CPF recCPF = new CPF("12345678910");
-        Email recEmail = new Email("rec.teste@gmail.com");
-
-        DadosPaciente novoPaciente1 = new DadosPaciente("João", "Salles", "12345678910",
-                "joao.teste@gmail.com", "32999881234",
-                "12345678", "rua teste", "bairro teste", "cidade teste", 12);
-
-        DadosPaciente novoPaciente2 = new DadosPaciente("Tom", "Jobim", "12345678910",
-                "tom.jobim@gmail.com", "32999123456",
-                "12345-678", "rua teste", "bairro teste", "cidade teste", 13);
-
-        DadosMedico novoMedico1 = new DadosMedico("Doutor", "House", "11111111111", "house123@gmail.com", "Clínico");
-
-        DadosMedico novoMedico2 = new DadosMedico("Jose", "Mauro", "22222222222", "jose.mauro@yahoo.com", "Pediatra");
-
-        DadosRecepcionista novoRecepcionista1 = new DadosRecepcionista("Thiago", "Silva", "12345678910", "Thiago45@gmail.com");
-
-        Recepcionista novoRecepcionista = new Recepcionista(recNome, recCPF, recEmail);
-        novoRecepcionista.cadastrarPaciente(novoPaciente1);
-        novoRecepcionista.cadastrarPaciente(novoPaciente2);
-        novoRecepcionista.cadastrarMedico(novoMedico1);
-        novoRecepcionista.cadastrarMedico(novoMedico2);
-        novoRecepcionista.cadastrarRecepcionista(novoRecepcionista1); 
-         */
-    }
+                /*
+                 * // TESTE DE PERSISTÊNCIA
+                 * Nome recNome = new Nome("Teste", "Recepcionista");
+                 * CPF recCPF = new CPF("12345678910");
+                 * Email recEmail = new Email("rec.teste@gmail.com");
+                 * 
+                 * DadosPaciente novoPaciente1 = new DadosPaciente("João", "Salles",
+                 * "12345678910",
+                 * "joao.teste@gmail.com", "32999881234",
+                 * "12345678", "rua teste", "bairro teste", "cidade teste", 12);
+                 * 
+                 * DadosPaciente novoPaciente2 = new DadosPaciente("Tom", "Jobim",
+                 * "12345678910",
+                 * "tom.jobim@gmail.com", "32999123456",
+                 * "12345-678", "rua teste", "bairro teste", "cidade teste", 13);
+                 * 
+                 * DadosMedico novoMedico1 = new DadosMedico("Doutor", "House", "11111111111",
+                 * "house123@gmail.com", "Clínico");
+                 * 
+                 * DadosMedico novoMedico2 = new DadosMedico("Jose", "Mauro", "22222222222",
+                 * "jose.mauro@yahoo.com", "Pediatra");
+                 * 
+                 * DadosRecepcionista novoRecepcionista1 = new DadosRecepcionista("Thiago",
+                 * "Silva", "12345678910", "Thiago45@gmail.com");
+                 * 
+                 * Recepcionista novoRecepcionista = new Recepcionista(recNome, recCPF,
+                 * recEmail);
+                 * novoRecepcionista.cadastrarPaciente(novoPaciente1);
+                 * novoRecepcionista.cadastrarPaciente(novoPaciente2);
+                 * novoRecepcionista.cadastrarMedico(novoMedico1);
+                 * novoRecepcionista.cadastrarMedico(novoMedico2);
+                 * novoRecepcionista.cadastrarRecepcionista(novoRecepcionista1);
+                 */
+        }
 }
--- src/main/java/br/ufjf/dcc/dcc025/view/RecepcionistaView.java
+++ src/main/java/br/ufjf/dcc/dcc025/view/RecepcionistaView.java
@@ -1,8 +1,17 @@
 package br.ufjf.dcc.dcc025.view;
 
 import javax.swing.*; //'*' importa todas as classes públicas desse pacote
-
 import java.awt.*;
+
+import br.ufjf.dcc.dcc025.model.Paciente;
+import br.ufjf.dcc.dcc025.model.valueobjects.Nome;
+import br.ufjf.dcc.dcc025.model.valueobjects.CPF;
+import br.ufjf.dcc.dcc025.model.valueobjects.Email;
+import br.ufjf.dcc.dcc025.model.valueobjects.Senha;
+import br.ufjf.dcc.dcc025.model.valueobjects.Contato;
+import br.ufjf.dcc.dcc025.model.valueobjects.Endereco;
+
+import br.ufjf.dcc.dcc025.controller.RecepcionistaController;
 
 public class RecepcionistaView extends JFrame {
 
@@ -11,12 +20,27 @@ public class RecepcionistaView extends JFrame {
     private JPanel painelDireito;
     private CardLayout cardLayout; // reutilizar a janela para as diversas opções
 
+    private RecepcionistaController controller;
+
     private JButton btnCadastroPaciente;
     private JButton btnStatus;
     private JButton btnListaMedico;
     private JButton btnAgendaMedico;
     private JButton btnConsultarFaltas;
     private JButton btnSair;
+    // cadastro paciente:
+    private JTextField txtNome;
+    private JTextField txtSobrenome;
+    private JTextField txtCpf;
+    private JTextField txtEmail;
+    private JPasswordField txtSenha;
+    private JTextField txtTelefone;
+    private JTextField txtRua;
+    private JTextField txtNumero;
+    private JTextField txtCidade;
+    private JTextField txtCep;
+    private JTextField txtBairro;
+    private JTextField txtNumeroCasa;
 
     public RecepcionistaView() {
         setTitle("Área Recepcionista");
@@ -93,17 +117,29 @@ public class RecepcionistaView extends JFrame {
     }
 
     private JPanel criarPainelCadastroPaciente() {
-        JPanel painel = new JPanel(new GridLayout(5, 2, 10, 10));
-        painel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        JPanel painel = new JPanel(new GridLayout(10, 4, 5, 5));
+        painel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
-        JTextField txtNome = new JTextField();
-        JTextField txtCpf = new JTextField();
-        JTextField txtEmail = new JTextField();
-        JPasswordField txtSenha = new JPasswordField();
+        txtNome = new JTextField();
+        txtSobrenome = new JTextField();
+        txtCpf = new JTextField();
+        txtEmail = new JTextField();
+        txtSenha = new JPasswordField();
+        txtTelefone = new JTextField();
+        txtRua = new JTextField();
+        txtNumero = new JTextField();
+        txtCidade = new JTextField();
+        txtCep = new JTextField();
+        txtBairro = new JTextField();
+        txtNumeroCasa = new JTextField();
+
         JButton btnSalvar = new JButton("Salvar");
 
         painel.add(new JLabel("Nome:"));
         painel.add(txtNome);
+
+        painel.add(new JLabel("Sobrenome:"));
+        painel.add(txtSobrenome);
 
         painel.add(new JLabel("CPF:"));
         painel.add(txtCpf);
@@ -114,10 +150,78 @@ public class RecepcionistaView extends JFrame {
         painel.add(new JLabel("Senha:"));
         painel.add(txtSenha);
 
+        painel.add(new JLabel("Telefone:"));
+        painel.add(txtTelefone);
+
+        painel.add(new JLabel("Rua:"));
+        painel.add(txtRua);
+        painel.add(new JLabel("Bairro:"));
+        painel.add(txtBairro);
+        painel.add(new JLabel("Número:"));
+        painel.add(txtNumero);
+        painel.add(new JLabel("CEP:"));
+        painel.add(txtCep);
+        painel.add(new JLabel("Cidade:"));
+        painel.add(txtCidade);
         painel.add(new JLabel());
         painel.add(btnSalvar);
 
+        btnSalvar.addActionListener(e -> salvarPaciente());
+
         return painel;
+    }
+
+    private void salvarPaciente() {
+        try {
+            Nome nome = new Nome(
+                    txtNome.getText(),
+                    txtSobrenome.getText());
+
+            CPF cpf = new CPF(txtCpf.getText());
+            Email email = new Email(txtEmail.getText());
+            Contato contato = new Contato(txtTelefone.getText());
+            Senha senha = new Senha(new String(txtSenha.getPassword()));
+
+            Endereco endereco = new Endereco(
+                    txtCep.getText(),
+                    txtRua.getText(),
+                    txtBairro.getText(),
+                    txtCidade.getText(),
+                    Integer.parseInt(txtNumeroCasa.getText()));
+
+            Paciente paciente = new Paciente(
+                    nome,
+                    cpf,
+                    email,
+                    contato,
+                    endereco);
+
+            controller.cadastrarPaciente(paciente);
+
+            JOptionPane.showMessageDialog(
+                    this,
+                    "Paciente cadastrado com sucesso!");
+
+            limparCampos();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(
+                    this,
+                    e.getMessage(),
+                    "Erro",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void limparCampos() {
+        txtNome.setText("");
+        txtCpf.setText("");
+        txtEmail.setText("");
+        txtSenha.setText("");
+        txtTelefone.setText("");
+        txtRua.setText("");
+        txtNumero.setText("");
+        txtCidade.setText("");
     }
 
 }
