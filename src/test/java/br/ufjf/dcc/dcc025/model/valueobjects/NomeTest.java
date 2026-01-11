f2fea1373391bdb10e30736888f512f91bef7931
Julia <juliacamposdp.gmail.com>
2026-01-11 15:20:39 -0300

    Teste de unidade - Nome

--- src/test/java/br/ufjf/dcc/dcc025/model/valueobjects/NomeTest.java
+++ src/test/java/br/ufjf/dcc/dcc025/model/valueobjects/NomeTest.java
@@ -1,5 +1,27 @@
 package br.ufjf.dcc.dcc025.model.valueobjects;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class NomeTest {
 
+    @Test
+    public void deveCriarNomeValido() {
+        Nome nome = new Nome("João", "Silva");
+
+        assertEquals("João", nome.getNome());
+        assertEquals("Silva", nome.getSobrenome());
+    }
+
+    @Test
+    public void devePermitirNomeVazio() {
+        Nome nome = new Nome("", "Silva");
+        assertEquals("", nome.getNome());
+    }
+
+    @Test
+    public void devePermitirSobrenomeVazio() {
+        Nome nome = new Nome("João", "");
+        assertEquals("", nome.getSobrenome());
+    }
 }
