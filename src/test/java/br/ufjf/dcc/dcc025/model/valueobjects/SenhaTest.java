85cd3d1875bed0d59421f4638970b3190ed01ab5
Julia <juliacamposdp.gmail.com>
2026-01-11 15:11:29 -0300

    Teste de unidade - Senha

--- src/test/java/br/ufjf/dcc/dcc025/model/valueobjects/SenhaTest.java
+++ src/test/java/br/ufjf/dcc/dcc025/model/valueobjects/SenhaTest.java
@@ -1,5 +1,20 @@
 package br.ufjf.dcc.dcc025.model.valueobjects;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import br.ufjf.dcc.dcc025.model.exception.InvalidPasswordException;
+
 public class SenhaTest {
 
+    @Test
+    public void deveCriarSenhaValida() {
+        Senha senha = new Senha("senha123");
+        assertNotNull(senha);
+    }
+
+    @Test(expected = InvalidPasswordException.class)
+    public void deveLancarExcecaoParaSenhaInvalida() {
+        new Senha("123");
+    }
 }
