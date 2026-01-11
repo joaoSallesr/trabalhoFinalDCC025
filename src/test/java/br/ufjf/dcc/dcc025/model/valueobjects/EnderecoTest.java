cd504fa6d5915b55bcb07751a1ddd00cf523f9be
Julia <juliacamposdp.gmail.com>
2026-01-11 17:26:43 -0300

    Teste de unidade - Endereço

--- src/test/java/br/ufjf/dcc/dcc025/model/valueobjects/EnderecoTest.java
+++ src/test/java/br/ufjf/dcc/dcc025/model/valueobjects/EnderecoTest.java
@@ -1,5 +1,34 @@
 package br.ufjf.dcc.dcc025.model.valueobjects;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import br.ufjf.dcc.dcc025.model.exception.InvalidAddressException;
+
 public class EnderecoTest {
 
+    @Test
+    public void deveCriarEnderecoValido() {
+        Endereco endereco = new Endereco(
+                "36000000",
+                "Rua Principal",
+                "Centro",
+                "Juiz de Fora",
+                100);
+
+        assertEquals("Juiz de Fora", endereco.getCidade());
+        assertEquals("Centro", endereco.getBairro());
+        assertEquals("Rua Principal", endereco.getRua());
+        assertEquals(100, endereco.getNumeroCasa());
+    }
+
+    @Test(expected = InvalidAddressException.class)
+    public void naoDeveAceitarCepInvalido() {
+        new Endereco(
+                "abc",
+                "Rua",
+                "Bairro",
+                "Cidade",
+                10);
+    }
 }
